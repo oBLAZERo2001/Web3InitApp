1247e152574356c0a14fffdeb8b2a0f1ae239c63
oBLAZERo2001 <hari9112001@gmail.com>
2023-08-26 12:16:01 +0530

    basic home init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home } from './screens/Home';
+import { Route } from 'react-router-dom/dist';
 
 function App() {
   return (
 		<Router>
 			<Routes>
-				{/* <Route path="/" exact element={<Home />} />
-				<Route path="/explore" exact element={<Explore />} />
+				<Route path="/" exact element={<Home />} />
+				{/* <Route path="/explore" exact element={<Explore />} />
 				<Route path="/profile/:user" exact element={<Profile />} /> */}
 			</Routes>
 		</Router>
