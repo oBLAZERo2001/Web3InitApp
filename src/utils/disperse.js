5f1b365e2ada2552c3de7d12ef49ef0e6b0f30bd
oBLAZERo2001 <hari9112001@gmail.com>
2023-08-26 17:04:25 +0530

    disperse token

--- src/utils/disperse.js
+++ src/utils/disperse.js
@@ -1,5 +1,6 @@
 import { getWalletAddress, switchChain } from "./wallet";
 import DisperseInterface from "../contracts/Disperse.json";
+import ERC20Interface from "../contracts/ERC20.json";
 import { CONTRACT_ADDRESS } from "../constants";
 import Web3 from "web3";
 import { split } from "./split";
@@ -44,4 +45,60 @@ export async function disperseNative(addressList) {
 			// 	}, 3000);
 			// });
 		});
+}
+
+export async function disperseToken(tokenAddress, addressList) {
+	await switchChain();
+	let { addArray, amtArray } = split(addressList);
+
+	const contract = new web3.eth.Contract(
+		DisperseInterface.abi,
+		CONTRACT_ADDRESS
+	);
+	const currentAddress = await getWalletAddress();
+
+	// Gas Calculation
+	console.log(addArray, amtArray);
+	const gasPrice = await web3.eth.getGasPrice();
+	const gas = await contract.methods
+		.disperseToken(tokenAddress, addArray, amtArray) // change 2 to price taken from field
+		.estimateGas({
+			from: currentAddress,
+		});
+
+	await contract.methods
+		.disperseToken(tokenAddress, addArray, amtArray) // change 2 to price taken from field
+		.send({
+			from: currentAddress,
+			gasPrice,
+			gas,
+		})
+		.on("transactionHash", function (hash) {})
+		.on("receipt", async function (receipt) {});
+}
+
+export async function allowToken(tokenAddress, addressList) {
+	await switchChain();
+	const { total } = split(addressList);
+
+	const contract = new web3.eth.Contract(ERC20Interface.abi, tokenAddress);
+	const currentAddress = await getWalletAddress();
+
+	// Gas Calculation
+	const gasPrice = await web3.eth.getGasPrice();
+	const gas = await contract.methods
+		.approve(CONTRACT_ADDRESS, Web3.utils.toWei(total.toString())) // change 2 to price taken from field
+		.estimateGas({
+			from: currentAddress,
+		});
+
+	await contract.methods
+		.approve(CONTRACT_ADDRESS, Web3.utils.toWei(total.toString())) // change 2 to price taken from field
+		.send({
+			from: currentAddress,
+			gasPrice,
+			gas,
+		})
+		.on("transactionHash", function (hash) {})
+		.on("receipt", async function (receipt) {});
 }
--- src/utils/split.js
+++ src/utils/split.js
@@ -1,23 +1,27 @@
 const UNIT = 1000000000000000000;
 
 export function split(t) {
-	let tempArray = t.split(/[\s,;:\t\r\n]+/);
+	try {
+		let tempArray = t.split(/[\s,;:\t\r\n]+/);
 
-	let oddArray = tempArray.filter((v, i) => i % 2);
-	let evenArray = tempArray.filter((v, i) => !(i % 2));
+		let oddArray = tempArray.filter((v, i) => i % 2);
+		let evenArray = tempArray.filter((v, i) => !(i % 2));
 
-	let addArray = [];
-	let amtArray = [];
+		let addArray = [];
+		let amtArray = [];
 
-	let total = 0.0;
+		let total = 0.0;
 
-	for (let i = 0; i < evenArray.length; ++i) {
-		if (!isNaN(oddArray[i]) && parseFloat(oddArray[i]) > 0.0) {
-			addArray.push(evenArray[i].trim());
-			amtArray.push((oddArray[i] * UNIT).toString());
-			total += parseFloat(oddArray[i]);
+		for (let i = 0; i < evenArray.length; ++i) {
+			if (!isNaN(oddArray[i]) && parseFloat(oddArray[i]) > 0.0) {
+				addArray.push(evenArray[i].trim());
+				amtArray.push((oddArray[i] * UNIT).toString());
+				total += parseFloat(oddArray[i]);
+			}
 		}
-	}
 
-	return { addArray, amtArray, total };
+		return { addArray, amtArray, total };
+	} catch (e) {
+		console.log(e);
+	}
 }
